d027ca732e4b7ef6b383034ce244fbb3260bd4a0
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-12-14 00:02:38 +0530

    Time: 8 ms (99.28%), Space: 93.4 MB (99.35%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
     long long findScore(vector<int>& nums) {
-        long long score = 0; // Change to long long to avoid overflow
+        long long score = 0; 
         int n = nums.size();
-        deque<int> q;
+        deque<int> dq;
 
-        // Traverse through the array
+
         for (int i = 0; i < n; i++) {
-            // If queue is not empty and the current number is greater than or equal to the last in queue
-            if (!q.empty() && nums[i] >= q.back()) {
+            if (!dq.empty()&&nums[i]>=dq.back()) {
                 bool skip = false;
-                // Process the elements in the queue
-                while (!q.empty()) {
-                    int add = q.back();
-                    q.pop_back();
-                    if (!skip) {
-                        score += add;
+                while (!dq.empty()) {
+                    int sum= dq.back();
+                    dq.pop_back();
+                    if(!skip) {
+                        score +=sum;
                     }
-                    skip = !skip;
+                    skip =!skip;
                 }
                 continue;
             }
-
-            // Add current element to the queue
-            q.push_back(nums[i]);
+            dq.push_back(nums[i]);
         }
-
-        // Final processing of remaining elements in the queue
         bool skip = false;
-        while (!q.empty()) {
-            int add = q.back();
-            q.pop_back();
+        while (!dq.empty()) {
+            int sum=dq.back();
+            dq.pop_back();
             if (!skip) {
-                score += add;
+                score+=sum;
             }
-            skip = !skip;
+            skip =!skip;
         }
 
         return score;
