d7cc39dfdfa9eea5df4de97f42987674a859f7de
Pranav Soan <pranav.soan.cs21@ggits.net>
2025-01-29 00:01:51 +0530

    Time: 16 ms (36.08%), Space: 110.4 MB (15.41%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -1,33 +1,48 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <queue>
 using namespace std;
 
 class Solution {
 public:
     int findMaxFish(vector<vector<int>>& grid) {
         int m = grid.size();
+        if (m == 0) return 0;
         int n = grid[0].size();
-        int maxFish = 0;
-        vector<vector<int>> directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        if (n == 0) return 0;
         
-        function<int(int, int)> dfs = [&](int r, int c) {
-            if (r < 0 || c < 0 || r >= m || c >= n || grid[r][c] <= 0) return 0;
-
-            int fishCaught = grid[r][c];
-            grid[r][c] = 0; 
-            for (const auto& dir : directions) {
-                fishCaught += dfs(r + dir[0], c + dir[1]);
-            }
-            return fishCaught;
-        };
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] > 0) {
-                    maxFish = max(maxFish, dfs(i, j));
+        int max_fish = 0;
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] > 0 && !visited[i][j]) {
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    visited[i][j] = true;
+                    int current_sum = 0;
+                    
+                    while (!q.empty()) {
+                        auto [r, c] = q.front();
+                        q.pop();
+                        current_sum += grid[r][c];
+                        
+                        for (auto& dir : directions) {
+                            int nr = r + dir.first;
+                            int nc = c + dir.second;
+                            
+                            if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc] && grid[nr][nc] > 0) {
+                                visited[nr][nc] = true;
+                                q.push({nr, nc});
+                            }
+                        }
+                    }
+                    
+                    max_fish = max(max_fish, current_sum);
                 }
             }
         }
-
-        return maxFish;
+        
+        return max_fish;
     }
 };
