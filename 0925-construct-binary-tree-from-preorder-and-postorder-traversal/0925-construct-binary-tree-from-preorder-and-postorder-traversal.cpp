4b95efe0d62594fa0c3a86e198e88c0aa6b6052a
Pranav Soan <pranav.soan.cs21@ggits.net>
2025-02-23 23:58:01 +0530

    Time: 0 ms (100%), Space: 28.3 MB (45.37%) - LeetHub

---
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        stack<TreeNode*> q;
+        TreeNode* root = new TreeNode(preorder[0]);
+        q.push(root);
+        int len = preorder.size();
+        vector<int> it(len+1);
+        
+        // Store postorder indices for quick lookup
+        for(int i = 0; i < len; i++){
+            it[postorder[i]] = i;
+        }
+
+        for(int i = 1; i < len; i++){
+            while(q.size() && it[preorder[i]] > it[q.top()->val]) {
+                q.pop();
+            }
+
+            TreeNode* curr = q.top();
+            TreeNode* node = new TreeNode(preorder[i]);
+            if (!curr->left) {
+                curr->left = node;
+            } else {
+                curr->right = node;
+            }
+            q.push(node);
+        }
+
+        return root;
+    }
+};
