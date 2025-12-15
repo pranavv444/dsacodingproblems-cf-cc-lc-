6234b221087bd63808a0ca0c08fc60dcffda4ef9
Pranav Soan <pranav.soan.cs21@ggits.net>
2025-12-15 22:11:50 +0530

    Time: 0 ms (100%), Space: 107.1 MB (24.91%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -5,20 +5,18 @@ public:
         int cnt = 1;
         
         for(int i = 0; i < prices.size(); i++){
-            // Handle the first element separately or simply skip checking prev
             if(i == 0){
                 ans += cnt;
                 continue;
             }
             
-            // Check if the current price is exactly 1 less than the previous
             if(prices[i] == prices[i-1] - 1){
                 cnt++;
             } else {
-                cnt = 1; // Reset sequence length
+                cnt = 1;
             }
             
-            ans += cnt; // Add the number of subarrays ending at current index
+            ans += cnt; 
         }
         
         return ans;
