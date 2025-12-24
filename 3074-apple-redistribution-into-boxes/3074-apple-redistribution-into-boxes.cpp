1040cfbc840233ef39f5d51474036bfc02e4d881
Pranav Soan <pranav.soan.cs21@ggits.net>
2025-12-25 01:28:24 +0530

    Time: 0 ms (100%), Space: 33.1 MB (77.48%) - LeetHub

---
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        int sum=0;
+        for(auto a:apple) sum+=a;
+        sort(capacity.begin(),capacity.end(),greater<>());
+        int res=0;
+        for(auto c:capacity){
+            sum-=c;
+            res+=1;
+            if(sum<=0) break;
+        }
+        return res;
+    }
+};
